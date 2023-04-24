4227e83e0b9aca8415bdfdde32b2b8bf8a1de05d
Zhe Pan <929675410@qq.com>
2023-04-25 00:05:33 +0800

    Update remodify.R

--- R/remodify.R
+++ R/remodify.R
@@ -30,7 +30,9 @@ myplotpara = function(dt,groupname){
   data = dt %>% filter(group == groupname)
   fit = cellsurvLQfit(data)
   para_group = plotPara(fit,group = groupname)
-  func_group = function(x){exp(para_group$rad_p[1] * x + para_group$rad_p[2] * x^2)}
+  b = para_group$rad_p[1]
+  a = para_group$rad_p[2]
+  func_group = function(x){exp(b * x + a * x^2)}
   mypara = list(group = groupname, fit = fit, para = para_group, func = func_group)
   return(mypara)
 }
